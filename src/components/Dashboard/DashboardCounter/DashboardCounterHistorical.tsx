90a0311c1774dd386a77a1bc0c664463a90efb36
Miftah Muhammad Farhan <miftahfarhan20@gmail.com>
2024-10-03 23:06:12 +0700

    feat: export dashboard

--- src/components/Dashboard/DashboardCounter/DashboardCounterHistorical.tsx
+++ src/components/Dashboard/DashboardCounter/DashboardCounterHistorical.tsx
@@ -1,42 +1,16 @@
-import { Button, Input, SimpleGrid, Stack } from "@chakra-ui/react";
-import { Icon } from "@iconify/react/dist/iconify.js";
-import React, { useRef, useState } from "react";
+import { Input, SimpleGrid, Stack } from "@chakra-ui/react";
+import React, { useState } from "react";
 import DashboardCounterData from "./DashboardCounterData";
 import useGetDashboard from "@/utils/hooks/dashboard/useGetDashboard";
-import { useReactToPrint } from "react-to-print";
+import ButtonExportDashboard from "../ButtonExportDashboard";
 
 export default function DashboardCounterHistorical() {
-  const componentPdf = useRef();
-
-  const [isExporting, setIsExporting] = useState(false);
   const [date, setDate] = useState("");
 
   const { data, refetch } = useGetDashboard({ dateProps: date });
 
-  const generatePDF = useReactToPrint({
-    // @ts-ignore
-    content: () => componentPdf.current,
-    documentTitle: "Dashboard Data",
-  });
-
-  const onExport = () => {
-    setIsExporting(true);
-
-    setTimeout(() => {
-      generatePDF();
-      setIsExporting(false);
-    }, 500);
-  };
-
   return (
     <>
-      <style type="text/css" media="print">
-        {
-          "\
-        @page { size: landscape; padding: 20px }\
-      "
-        }
-      </style>
       <Stack direction="column" spacing="20px">
         <Stack
           direction="row"
@@ -52,23 +26,12 @@ export default function DashboardCounterHistorical() {
             }}
             type="date"
           />
-          <Button
-            leftIcon={<Icon icon="bx:download" />}
-            variant="green-solid-medium"
-            width="100%"
-            onClick={onExport}
-            isLoading={isExporting}
-          >
-            Export Data
-          </Button>
+          <ButtonExportDashboard date={date} />
         </Stack>
 
         <SimpleGrid
-          columns={isExporting ? 5 : [2, 2, 3, 5]}
+          columns={[2, 2, 3, 5]}
           gap={["10px", "10px", "20px", "20px"]}
-          p={isExporting ? "20px" : "0px"}
-          // @ts-ignore
-          ref={componentPdf}
         >
           <DashboardCounterData data={data} />
         </SimpleGrid>
